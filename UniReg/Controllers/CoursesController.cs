948b6ab36f1a9ad6f27cce7827ab9312da742f44
Garrett <gman9mm@live.com>
2021-01-04 14:27:54 -0800

    Add join functionality to registrar, add basic views.

--- UniReg/Controllers/CoursesController.cs
+++ UniReg/Controllers/CoursesController.cs
@@ -1,74 +1,75 @@
-// using Microsoft.AspNetCore.Mvc;
-// using UniReg.Models;
-// using System.Collections.Generic;
-// using System.Linq;
-// using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using UniReg.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
-// namespace UniReg.Controllers
-// {
-//     public class CoursesController : Controller
-//     {
-//         private readonly UniRegContext _db;
+namespace UniReg.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly UniRegContext _db;
 
-//         public CoursesController(UniRegContext db)
-//         {
-//             _db = db;
-//         }
+        public CoursesController(UniRegContext db)
+        {
+            _db = db;
+        }
 
-//         public ActionResult Index()
-//         {
-//             List<Course> model = _db.Courses.ToList();
-//             return View(model);
-//         }
+        public ActionResult Index()
+        {
+            List<Course> model = _db.Courses.ToList();
+            return View(model);
+        }
 
-//         public ActionResult Create()
-//         {
-//             return View();
-//         }
+        public ActionResult Create()
+        {
+            return View();
+        }
 
-//         [HttpPost]
-//         public ActionResult Create(Course course)
-//         {
-//             _db.Courses.Add(course);
-//             _db.SaveChanges();
-//             return RedirectToAction("Index");
-//         }
+        [HttpPost]
+        public ActionResult Create(Course course)
+        {
+            _db.Courses.Add(course);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
-//         public ActionResult Details(int id)
-//         {
-//             Course thisCourse = _db.Courses
-//             .Include(course => course.Students)
-//             .FirstOrDefault(course => course.CourseId == id);
-//             return View(thisCourse);
-//         }
+        public ActionResult Details(int id)
+        {
+            var thisCourse = _db.Courses
+            .Include(course => course.Students)
+            .ThenInclude(join => join.Student)
+            .FirstOrDefault(course => course.CourseId == id);
+            return View(thisCourse);
+        }
 
-//         public ActionResult Edit(int id)
-//         {
-//             var thisCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
-//             return View(thisCourse);
-//         }
+        public ActionResult Edit(int id)
+        {
+            var thisCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
+            return View(thisCourse);
+        }
 
-//         [HttpPost]
-//         public ActionResult Edit(Course Course)
-//         {
-//             _db.Entry(Course).State = EntityState.Modified;
-//             _db.SaveChanges();
-//             return RedirectToAction("Index");
-//         }
+        [HttpPost]
+        public ActionResult Edit(Course Course)
+        {
+            _db.Entry(Course).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
-//         public ActionResult Delete(int id)
-//         {
-//             var thisCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
-//             return View(thisCourse);
-//         }
+        public ActionResult Delete(int id)
+        {
+            var thisCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
+            return View(thisCourse);
+        }
 
-//         [HttpPost, ActionName("Delete")]
-//         public ActionResult DeleteConfirmed(int id)
-//         {
-//             var thisCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
-//             _db.Courses.Remove(thisCourse);
-//             _db.SaveChanges();
-//             return RedirectToAction("Index");
-//         }
-//     }
-// }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var thisCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
+            _db.Courses.Remove(thisCourse);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- UniReg/Controllers/StudentsController.cs
+++ UniReg/Controllers/StudentsController.cs
@@ -38,6 +38,24 @@ namespace UniReg.Controllers
             return RedirectToAction("Index");
         }
 
+        public ActionResult AddCourse(int id)
+        {
+            var thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
+            ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "Name");
+            return View(thisStudent);
+        }
+
+        [HttpPost]
+        public ActionResult AddCourse(Student student, int CourseId)
+        {
+            if (CourseId != 0)
+            {
+                _db.CourseStudent.Add(new CourseStudent() { CourseId = CourseId, StudentId = student.StudentId });
+            }
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
         public ActionResult Details(int id)
         {
             var thisStudent = _db.Students
@@ -47,37 +65,50 @@ namespace UniReg.Controllers
             return View(thisStudent);
         }
 
-//         public ActionResult Edit(int id)
-//         {
-//             var thisStudent = _db.Students.FirstOrDefault(Students => students.StudentId == id);
-//             ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "Name");
-//             return View(thisStudent);
-//         }
+        public ActionResult Edit(int id)
+        {
+            var thisStudent = _db.Students.FirstOrDefault(Students => students.StudentId == id);
+            ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "Name");
+            return View(thisStudent);
+        }
 
-//         [HttpPost]
-//         public ActionResult Edit(Student student)
-//         {
-//             _db.Entry(student).State = EntityState.Modified;
-//             _db.SaveChanges();
-//             return RedirectToAction("Index");
-//         }
+        [HttpPost]
+        public ActionResult Edit(Student student)
+        {
+            if (CourseId != 0)
+            {
+                _db.CourseStudent.Add(new CourseStudent() { CourseId = CourseId, StudentId = student.StudentId });
+            }
+            _db.Entry(student).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
-//         public ActionResult Delete(int id)
-//         {
-//             var thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
-//             return View(thisStudent);
-//         }
+        public ActionResult Delete(int id)
+        {
+            var thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
+            return View(thisStudent);
+        }
 
-//         [HttpPost, ActionName("Delete")]
-//         public ActionResult DeleteConfirmed(int id)
-//         {
-//             var thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
-//             _db.Students.Remove(thisStudent);
-//             _db.SaveChanges();
-//             return RedirectToAction("Index");
-//         }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
+            _db.Students.Remove(thisStudent);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
-//     }
+        [HttpPost]
+        public ActionResult DeleteCourse(int joinId)
+        {
+            var joinEntry = _db.CourseStudent.FirstOrDefault(joinEntry => joinEntry.CourseStudentId == joinId);
+            _db.CourseStudent.Remove(joinEntry);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
 
 
-// }
+}
